d0fdb37e4d1e3a9cc11af9d0b71a181085842907
cybermelon <lhdff552@duck.com>
2022-11-18 02:43:50 -0500

    remove unnecessary stuff

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,21 +2,16 @@
   import '../app.css';
 
   export const prerender = true;
+
+  // Put the following line into <style> to use a google font
+  // @import url(//fonts.googleapis.com/css?family=Oswald);
 </script>
 
 <svelte:head>
   <link rel="icon" href="%sveltekit.assets%/favicon.png" />
-  <script src="https://kit.fontawesome.com/d28d35f71e.js" crossorigin="anonymous"></script>
 </svelte:head>
 
 
 <slot />
 
 
-<style>
-  @import url(//fonts.googleapis.com/css?family=Oswald);
-  @import url(//fonts.googleapis.com/css?family=Source+Serif+4);
-  @import url(//fonts.googleapis.com/css?family=Zen+Old+Mincho);
-  @import url(//fonts.googleapis.com/css?family=BIZ+UDMincho);
-  @import url(//fonts.googleapis.com/css?family=Abel);
-</style>
